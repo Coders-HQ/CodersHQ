b78a369ea4b2c83e1817dcf966783c228d347a01
rashed <x.suwaidi@gmail.com>
2021-02-12 15:53:12 +0400

    removed github id from settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -303,8 +303,8 @@ SOCIALACCOUNT_PROVIDERS = {
         # (``socialaccount`` app) containing the required client
         # credentials, or list them here:
         'APP': {
-            'client_id': 'c3af5fcd8de5fe4cff35',
-            'secret': 'f3b86d9f9a4330b18c0c484b5870c7fa02f725b6',
+            'client_id': env("GITHUB_CLIENT_ID"),
+            'secret': env("GITHUB_CLIENT_SECRET"),
             'key': ''
         }
     }
