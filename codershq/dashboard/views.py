8140c908e38b42c6fd19f4001389a44ebe1bb453
rashed <x.suwaidi@gmail.com>
2021-02-15 18:07:43 +0400

    add profile pics

---
+++ codershq/dashboard/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def index(request):
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # context = {'latest_question_list': latest_question_list}
+    return render(request, 'dashboard/home.html')
