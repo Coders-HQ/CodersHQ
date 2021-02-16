dbf3e4e3e1b389ef8e390e286f50472f5df16b44
rashed <x.suwaidi@gmail.com>
2021-02-16 20:09:09 +0400

    news

--- codershq/dashboard/views.py
+++ codershq/dashboard/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+import requests
+
+news_url = 'https://www.reddit.com/r/programming/.json'
 
 def index(request):
-    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # context = {'latest_question_list': latest_question_list}
     return render(request, 'dashboard/home.html')
+
+def news(request):
+    resp = requests.get(url=news_url, headers = {'User-agent': 'your bot 0.1'})
+    body = resp.json() 
+    context = {'body': body['data']['children']}
+    return render(request, 'dashboard/news.html', context)
+
