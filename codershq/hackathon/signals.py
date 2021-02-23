c02f7043cb3ea15ec614318ec032ee0896474d2c
rashed <x.suwaidi@gmail.com>
2021-02-23 20:23:50 +0400

    email only once

--- codershq/hackathon/signals.py
+++ codershq/hackathon/signals.py
@@ -5,14 +5,15 @@ from django.core.mail import send_mail
 
 
 def send_hackathon_mail(sender, **kwargs):
-    email = EmailAddress.objects.get(pk=list(kwargs['pk_set'])[0])
-    send_mail(
-        'Subject here',
-        'Here is the message.',
-        'from@example.com',
-        [email],
-        fail_silently=True,
-    )
+    if kwargs.get('action') == 'post_add': 
+        email = EmailAddress.objects.get(pk=list(kwargs['pk_set'])[0])
+        send_mail(
+            'Subject here',
+            'Here is the message.',
+            'from@example.com',
+            [email],
+            fail_silently=True,
+        )
 
 
 m2m_changed.connect(send_hackathon_mail, sender=Hackathon.competitors.through)
