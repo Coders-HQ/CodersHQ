2c6d08ce89f95de2c8cf38c9a690c44078d62fb7
rashed <x.suwaidi@gmail.com>
2021-02-27 09:50:06 +0400

    slack integration, create channel on new hackathon

--- codershq/hackathon/signals.py
+++ codershq/hackathon/signals.py
@@ -1,8 +1,10 @@
 from django.db.models.signals import m2m_changed
 from codershq.hackathon.models import Hackathon
 from allauth.account.models import EmailAddress
+from django.db.models.signals import post_save
 from django.core.mail import send_mail
-
+from django.dispatch import receiver
+from codershq.hackathon.slack import create_channel
 
 def send_hackathon_mail(sender, **kwargs):
     if kwargs.get('action') == 'post_add': 
@@ -14,6 +16,10 @@ def send_hackathon_mail(sender, **kwargs):
             [email],
             fail_silently=True,
         )
-
-
+    
 m2m_changed.connect(send_hackathon_mail, sender=Hackathon.competitors.through)
+
+@receiver(post_save, sender=Hackathon)
+def create_slack_channel(sender, instance, created, **kwargs):
+    if created:
+        create_channel(instance.slug)
