c293ba6188af7634101252415fd1aa6f93002dd9
git-shad0w <aboodshadow@outlook.com>
2021-03-09 11:53:54 +0400

    Upgraded jQuery to version 3.6.0

--- codershq/static/js/project.js
+++ codershq/static/js/project.js
@@ -9,26 +9,6 @@ var theme = window.localStorage.getItem("preferredTheme");
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
   })
-
-  var rangeSlider = function(){
-    var slider = $('.range-slider'),
-        range = $('.range-slider__range'),
-        value = $('.range-slider__value');
-      
-    slider.each(function(){
-  
-      value.each(function(){
-        var value = $(this).prev().attr('value');
-        $(this).html(value);
-      });
-  
-      range.on('input', function(){
-        $(this).next(value).html(this.value);
-      });
-    });
-  };
-  
-  rangeSlider();
   
 var tooltipTriggerList = [].slice.call(
   document.querySelectorAll('[data-bs-toggle="tooltip"]')
