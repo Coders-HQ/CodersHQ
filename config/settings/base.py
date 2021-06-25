e1b85eb7be847ded85b171e84a51a1038011a448
rashed <x.suwaidi@gmail.com>
2021-06-25 10:56:09 +0400

    added musab as admin

--- config/settings/base.py
+++ config/settings/base.py
@@ -225,7 +225,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""Rashed Al Suwaidi""", "x.suwaidi@gmail.com")]
+ADMINS = [("""Rashed Al Suwaidi""", "x.suwaidi@gmail.com"), ("""Musab Obaid""", "musab.obaid@gmail.com")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
