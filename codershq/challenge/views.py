e9d6fa3c02e0d0f7ad6d8cbe54da060dc6351871
rashed <x.suwaidi@gmail.com>
2021-08-02 20:44:32 +0400

    update challenge view

--- codershq/challenge/views.py
+++ codershq/challenge/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponseForbidden, HttpResponseRedirect
-from django.views.generic import DetailView, ListView
+from django.views.generic import ListView, DetailView
 from django.urls import reverse
 from codershq.challenge.models import Challenge
 from django.utils import timezone
@@ -14,23 +13,23 @@ class ChallengeList(LoginRequiredMixin, ListView):
     paginate_by = 10
 
 
-# class ChallengeDetail(LoginRequiredMixin, DetailView):
-#     model = Challenge
+class ChallengeDetail(LoginRequiredMixin, DetailView):
+    model = Challenge
 
-#     def post(self, request, *args, **kwargs):
+    # def post(self, request, *args, **kwargs):
 
-#         # current challenge instance
-#         self.object = self.get_object()
+    #     # current challenge instance
+    #     self.object = self.get_object()
 
-#         # add user if not exists
-#         # if user exists remove user
-#         if request.user in self.object.competitors.all():
-#             self.object.competitors.remove(request.user)
-#             messages.warning(request, 'You are removed from this challenge')
+    #     # add user if not exists
+    #     # if user exists remove user
+    #     if request.user in self.object.competitors.all():
+    #         self.object.competitors.remove(request.user)
+    #         messages.warning(request, 'You are removed from this challenge')
 
-#         else:
-#             if timezone.now().date() < self.object.last_join_date:
-#                 self.object.competitors.add(request.user)
-#                 messages.success(request, 'You were successfully added to the challenge')
+    #     else:
+    #         if timezone.now().date() < self.object.last_join_date:
+    #             self.object.competitors.add(request.user)
+    #             messages.success(request, 'You were successfully added to the challenge')
 
-#         return HttpResponseRedirect(reverse('challenge:detail', kwargs={'slug': self.object.slug}))
+    #     return HttpResponseRedirect(reverse('challenge:detail', kwargs={'slug': self.object.slug}))
