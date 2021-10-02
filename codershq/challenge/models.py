431dbee9433dfae1a62c85868b192d05c32ce701
marrwin <68703246+marrwin@users.noreply.github.com>
2021-10-02 21:43:18 +0400

    Add files via upload

--- codershq/challenge/models.py
+++ codershq/challenge/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.fields import DateTimeField
 from django.utils.translation import gettext_lazy as _
+from codershq.users.models import Team, User
 
 
 class Challenge(models.Model):
@@ -12,13 +13,20 @@ class Challenge(models.Model):
     logo = models.ImageField(_("Challenge Logo"), upload_to='challenges/logo/')
     github_link = models.TextField(_("Challenge github link"), default=None)
     website = models.TextField(_("Website link"), max_length=100)
-    slack_group = models.TextField(_("Slack group"), default=None, max_length=100)
-    cloud_provider = models.TextField(_("Cloud Provider"), default=None, max_length=100)
-    cloud_provider_url = models.URLField(_("Cloud Provider URL"), default=None)
-    cloud_provider_token = models.TextField(_("Cloud Provider token"), default=None, max_length=500)
+    slack_group = models.TextField(_("Slack group"), null=True, blank=True, max_length=100)
+    cloud_provider = models.TextField(_("Cloud Provider"), null=True, blank=True, max_length=100)
+    cloud_provider_url = models.URLField(_("Cloud Provider URL"), null=True, blank=True)
+    cloud_provider_token = models.TextField(_("Cloud Provider token"), null=True, blank=True, max_length=500)
+    prticipant_teams = models.ManyToManyField(Team, blank=True)
+
 
     def __str__(self) -> str:
-        return "Challenge: " + self.title
+        return "Challenge: " + self.name
+
+class SubmittedCode(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
+    code_file = models.FileField(upload_to='challenges/code_files/')
 
 
 class Sprint(models.Model):
