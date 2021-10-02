8fc75b78897459b5e71bdae7cd50769490d3cd4b
marrwin <68703246+marrwin@users.noreply.github.com>
2021-10-02 21:40:27 +0400

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from codershq.challenge.models import Challenge, SubmittedCode
+
+
+
+class CodeAdmin(admin.ModelAdmin):
+    list_filter = ['user', 'challenge']
+
+
+
+
+admin.site.register(SubmittedCode, CodeAdmin)
+admin.site.register(Challenge)
