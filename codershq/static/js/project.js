1edd99c8b6be1d2f858e31aab570344e0126d323
rashed <x.suwaidi@gmail.com>
2021-12-16 14:30:20 +0400

    fix broken gulp

---
+++ codershq/static/js/project.js
@@ -0,0 +1,18 @@
+var theme = window.localStorage.getItem("preferredTheme");
+
+ /* Project specific Javascript goes here. */
+ (function () {
+   $('[data-toggle="tooltip"]').tooltip();
+ });
+
+   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+     return new bootstrap.Tooltip(tooltipTriggerEl)
+   })
+
+ var tooltipTriggerList = [].slice.call(
+   document.querySelectorAll('[data-bs-toggle="tooltip"]')
+ );
+ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+   return new bootstrap.Tooltip(tooltipTriggerEl);
+ });
