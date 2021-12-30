3d63f2d00849a1a9b24906f408ec06fb7f99341c
rashed <x.suwaidi@gmail.com>
2021-12-30 10:48:35 +0400

    events example

--- codershq/events/views.py
+++ codershq/events/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the events index.")
+    return render(request, 'events/main.html')
