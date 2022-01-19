87a649cc48596e283d6e7376e908f9de790e223c
rashed <x.suwaidi@gmail.com>
2022-01-19 19:57:15 +0400

    fix event bug and allow no participants

--- codershq/events/models.py
+++ codershq/events/models.py
@@ -15,17 +15,17 @@ class Event(models.Model):
     image = models.ImageField(_("Event image"), upload_to=event_image_path)
     date_time = models.DateTimeField(_("Event date and time"))
     duration = models.IntegerField(_("Event duration (hrs)"), null=True, blank=True)
-    short_description = models.CharField(_("Short event description"), max_length=500, default=None), 
     description = RichTextField()
+    short_description = models.CharField(_("Short event description"), max_length=500, default=None)
     event_link = models.URLField(_("Event zoom link (only if online)"), blank=True, null=True)
     event_location = models.CharField(_("Event location (use 'Online' if its online)"),
                                       max_length=150, blank=True, null=True)
     seats = models.IntegerField(_("Number of seats available (if not online"), blank=True, null=True)
 
     # people who are intrested in the event
-    attendees = models.ManyToManyField(User, related_name='attended_events')
+    attendees = models.ManyToManyField(User, related_name='attended_events', blank=True)
     # people who actually joined the event
-    participants = models.ManyToManyField(User, related_name='participated_events')
+    participants = models.ManyToManyField(User, related_name='participated_events', blank=True)
 
     def __str__(self):
         return self.title
