47d6bf1b2df1257fd27f65dd5f892f507555ba63
rashed <x.suwaidi@gmail.com>
2022-02-02 13:49:02 +0400

    update redirect after login

--- codershq/dashboard/views.py
+++ codershq/dashboard/views.py
@@ -6,7 +6,10 @@ news_url = "https://www.reddit.com/r/programming/.json"
 
 
 def index(request):
-    return redirect("https://ai.gov.ae/codershq")
+    if not request.user.is_authenticated:
+        return redirect("https://ai.gov.ae/codershq")
+
+    return render(request, "pages/underConstruction.html")
 
 
 def landing(request):
