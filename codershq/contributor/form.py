178c8e2f0ee948098d125a53c2854306a359d991
Saeed Ali Hassan <sh357500@gmail.com>
2022-03-30 11:33:26 +0400

    Form Fix
    d

--- codershq/contributor/form.py
+++ codershq/contributor/form.py
@@ -1,9 +1,10 @@
 from django import forms
+from django.contrib.postgres.forms import SimpleArrayField
 
 
 class ContributorForm(forms.Form):
     name = forms.CharField(label="Contributor Name", max_length=200)
-    role = forms.EmailField(label="Contributor Role (Comma Seperated)")
+    role = forms.SimpleArrayField(forms.CharField((label="Contributor Role (Comma Seperated)")))
     image = forms.ImageField(label="Contributor Image")
  	github = forms.URLField(label="Github URL")
  	twitter = forms.URLField(label="Twitter URL")
