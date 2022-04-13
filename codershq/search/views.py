e33af26958088bb1618ea46f55fa800401ad75ed
Mohammed Sadiq Bagalkot <mb200@hw.ac.uk>
2022-04-13 20:38:14 +0400

    Query parameters for search

--- codershq/search/views.py
+++ codershq/search/views.py
@@ -4,6 +4,7 @@ def search(request):
     if 'search' in request.GET:
         search = request.GET.get('search')
         search_select = request.GET.get('search_select')
-        return render(request, 'pages/search.html', {'search': search, 'search_select': search_select})
+        skill = request.GET.getlist('skill')
+        return render(request, 'pages/search.html', {'search': search, 'search_select': search_select, 'skill': skill})
     else:
         return render(request, 'pages/search.html', {})
