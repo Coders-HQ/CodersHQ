2213c6416ebea551f0b67050d01659ee90e25399
Musab0 <musab.obaid@gmail.com>
2022-05-27 03:13:06 +0400

    create assessment django app

---
+++ codershq/assessment/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AssessmentConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = "codershq.assessment"
+    verbose_name = ("Assessment")
---
+++ codershq/assessment/views.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.views.generic import TemplateView
+from djangosaml2idp.models import ServiceProvider
+
+
+class IndexView(TemplateView):
+    template_name = "assessment/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context.update({
+            "logout_url": settings.LOGOUT_URL,
+            "login_url": settings.LOGIN_URL,
+        })
+        if self.request.user.is_authenticated:
+            context.update({
+                "known_sp_ids": [sp for sp in ServiceProvider.objects.filter(active=True)],
+            })
+        return context
