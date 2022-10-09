7f4498122e9beeba47c52b8096b1f70449a5dfdd
Musab0 <musab.obaid@gmail.com>
2022-10-10 02:08:14 +0400

    polish

--- codershq/portfolio/models.py
+++ codershq/portfolio/models.py
@@ -15,17 +15,14 @@ class Portfolio(models.Model):
     """
     Portfolio model
     """
-
     GENDER_CHOICES = (
         ('M', 'Male'),
         ('F', 'Female'),
-        
     )
     EMPLOYMENT_TIME_CHOICES = (
         ('F', 'Fulltime'),
         ('P', 'Part-Time'),
     )
-
     first_name = models.CharField(_("First Name"), blank=False, max_length=255)
     last_name = models.CharField(_("Last Name"), blank=False, max_length=255)
     gender = models.CharField(
@@ -43,17 +40,15 @@ class Portfolio(models.Model):
         blank_label='(select country)',
         blank=False,
     )
+    academic_qualification = models.CharField(
+        _("Your highest academic qualification"), blank=False, max_length=30
+    )
     mobile_number = models.CharField(
         _("Your mobile number"),
         max_length=100,
         blank=True,
         null=True,
         help_text=_("(eg: +97150XXXXXXX)"))
-    about = models.TextField(
-        _("About"),
-        blank=True,
-        help_text=_("Tell us a bit about yourself"),
-        max_length=1500)
     is_seeking_job = models.BooleanField(
         _("Seeking employment?"),
         default=False,
@@ -68,7 +63,22 @@ class Portfolio(models.Model):
         blank=True,
         null=True,
         help_text=_("(If currently employed)"))
-
+    years_experience = models.IntegerField(
+        _("Years of experience"),
+        null=True,
+        blank=True,
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(0)
+        ]
+    )
+    employment_time = models.CharField(
+        _("Would you prefer to work full-time or part-time?"),
+        null=True,
+        blank=True,
+        max_length=1,
+        choices=EMPLOYMENT_TIME_CHOICES,
+    )
     github = models.CharField(
         _("Github account"),
         max_length=100,
@@ -83,53 +93,39 @@ class Portfolio(models.Model):
         blank=True,
         null=True,
         help_text=_("https://linkedin.com/in/XXXX"))
-    twitter = models.CharField(
-        _("Twitter account"),
-        max_length=100,
-        validators=[URLValidator],
-        blank=True,
-        null=True,
-        help_text=_("https://twitter.com/coders_hq"))
-
-    fav_language = models.CharField(
-        _("Whats your favourite language?"),
-        help_text=_("This can be a language or framework"),
-        null=True,
-        blank=True,
-        max_length=50)
-    employment_time = models.CharField(
-        _("Do you like to work full-time or part-time?"),
-        null=True,
-        blank=True,
-        max_length=1,
-        choices=EMPLOYMENT_TIME_CHOICES,
-    )
+    # twitter = models.CharField(
+    #     _("Twitter account"),
+    #     max_length=100,
+    #     validators=[URLValidator],
+    #     blank=True,
+    #     null=True,
+    #     help_text=_("https://twitter.com/coders_hq"))
     personal_site = models.URLField(
         _("Do you have a personal site?"),
         null=True,
         validators=[URLValidator],
         blank=True,
     )
+    fav_language = models.CharField(
+        _("Whats your favourite language?"),
+        help_text=_("This can be a language or framework"),
+        null=True,
+        blank=True,
+        max_length=50)
+    about = models.TextField(
+        _("About"),
+        blank=True,
+        help_text=_("Tell us a bit about yourself"),
+        max_length=1500)
+    
     proud_project = models.TextField(
         _("Tell us about the project that you are most proud of"),
         blank=True,
         help_text=_("This can be any project that you were involved in"),
         max_length=2500)
-    academic_qualification = models.CharField(
-        _("Your highest academic qualification"), blank=True, max_length=30
-    )
-    profile_image = models.ImageField(
-        _("Profile image"), upload_to=user_image_path, null=True, blank=True
-    )
-    years_experience = models.IntegerField(
-        _("Years of experience"),
-        null=True,
-        blank=True,
-        validators=[
-            MaxValueValidator(100),
-            MinValueValidator(0)
-        ]
-    )
+    # profile_image = models.ImageField(
+    #     _("Profile image"), upload_to=user_image_path, null=True, blank=True
+    # )
 
     # auto generated fields
     created_at = models.DateTimeField(auto_now_add=True)
