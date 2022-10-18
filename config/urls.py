977ad66fd2270e9c38fb13fff2311b5bd636ddaf
rashed <x.suwaidi@gmail.com>
2022-10-18 18:22:53 +0400

    add account email confirmation

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,4 @@
+from allauth.account.views import confirm_email
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -55,8 +56,10 @@ urlpatterns = [
 
     #  API
     path('api-auth/', include('rest_framework.urls')),
+    path('api/rest-auth/registration/account-confirm-email/<str:key>/', confirm_email, name='account_confirm_email'),
     path('api/rest-auth/', include('rest_auth.urls')),
     path('api/rest-auth/registration/', include('rest_auth.registration.urls')),
+
     path('api-token-auth/', obtain_jwt_token),
     path("api/", include("codershq.api.urls", namespace="api")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
