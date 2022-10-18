eab133f14df64f92e33c06beb23b741f51471574
rashed <x.suwaidi@gmail.com>
2022-10-18 16:46:16 +0400

    add urls

--- codershq/api/urls.py
+++ codershq/api/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
 # assessment results
 urlpatterns += [
     path("users/all/", views.users_all, name="users_all"),
+    path("user/<str:username>/", views.user, name="user"),
     path("assessment/skills/all/", views.skills_all, name="skills_all"),
     path("users/skills/<int:id>/", views.user_id_skills, name="skill_id"),
     path("assessment/analytics/", views.analytics_public, name="analytics_pub"),
